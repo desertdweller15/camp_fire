72b6f53ada1c7e3d90a34c50571b974073b18f81
desertdweller15 <bcoutee11@gmail.com>
2016-08-14 15:58:19 -0700

    Finalized SPL

--- camp_fire/app.js
+++ camp_fire/app.js
@@ -226,3 +226,117 @@ for (var i = 0; i < hours.length; i++) {
     // this.ulEl.appendChild(liEl[18]);
 
 CapitolHill.render();
+
+var SeattlePublicLibrary = {
+  name: 'Seattle Public Library',
+  minCustomer: 9,
+  maxCustomer: 45,
+  averageCups: 2.6,
+  averagePound: 0.02,
+  customerHour: [],  //Number of customers per hour
+  cupsHr: [],  // Cups sold per hour
+  pndHR: [],  //Pounds to-go sold per hour
+  pndPerCup: [], //Pounds of beans used for each cup per hour
+  netPnd: [],   //Pounds of beans used for both per hour
+  numEmp: [],  //Number of employees needed per hour
+  totalCups: 0,  //Total cups sold for the day
+  totalPnd: 0,  //Total pounds to-go sold for the entire day
+  totalNetPnd: 0,  //Total pounds of beans sold for the entire day
+  totalCust: 0, //Total number of customers for the entire day
+  ulEl: document.getElementById('SeattlePublicLibrary'),
+
+  randomCustHr: function() {
+    for (var i = 0; i < hours.length; i++) {
+      this.customerHour[i] = Math.floor(Math.random() * (this.maxCustomer - this.minCustomer) + this.minCustomer);
+    }
+  },
+  cupsHr: function() {
+    for (var i = 0; i < hours.length; i++) {
+      this.cupsHr[i] = parseFloat((this.customerHour[i] * this.averageCups).toFixed(2));
+    }
+  },
+  totalCupsCalc: function() {
+    for (var i = 0; i < hours.length; i++) {
+      this.totalCups += this.cupsHr[i];
+    }
+    this.totalCups = parseFloat(this.totalCups.toFixed(2));
+  },
+  pndHr: function() {
+    for (var i = 0; i < hours.length; i++) {
+      this.pndHR[i] = parseFloat((this.customerHour[i] * this.averagePound).toFixed(2));
+    }
+  },
+  totalPndCalc: function() {
+    for (var i = 0; i < hours.length; i++) {
+      this.totalPnd += this.pndHR[i];
+    }
+    this.totalPnd = parseFloat(this.totalPnd.toFixed(2));
+  },
+  pndPerCup: function() {
+    for (var i = 0; i < hours.length; i++) {
+      this.pndPerCup[i] = parseFloat((this.cupsHr[i] / 16).toFixed(2));
+    }
+  },
+  netPndCalc: function() {
+    for (var i = 0; i < hours.length; i++) {
+      this.netPnd[i] = parseFloat((this.pndHR[i] + this.pndPerCup[i]).toFixed(2));
+    }
+  },
+  totalNetPndCalc: function() {
+    for (var i = 0; i < hours.length; i++) {
+      this.totalNetPnd += this.netPnd[i];
+    }
+    this.totalNetPnd = parseFloat(this.totalNetPnd.toFixed(2));
+  },
+  totalCustCalc: function() {
+    for (var i = 0; i < hours.length; i++) {
+      this.totalCust += this.customerHour[i];
+    }
+  },
+  numEmpCalc: function() {
+    for (var i = 0; i < hours.length; i++) {
+      this.numEmp[i] = Math.ceil(this.customerHour[i] / 30);
+    }
+  },
+  render: function() {
+    for (var i = 0; i < hours.length; i++) {
+      var liEl = document.createElement('li');
+      liEl.textContent = hours[i] + ': ' + this.netPnd[i] + ' lbs [' + this.customerHour[i] + ' customers, ' + this.cupsHr[i] + ' cups (' + this.pndPerCup[i] + ' lbs), ' + this.pndHR[i] + ' lbs to-go]';
+      this.ulEl.appendChild(liEl);
+    }
+  }
+};
+// Calling out all the methods for Seattle Public Library object
+SeattlePublicLibrary.randomCustHr();
+SeattlePublicLibrary.cupsHr();
+SeattlePublicLibrary.totalCupsCalc();
+SeattlePublicLibrary.pndHr();
+SeattlePublicLibrary.totalPndCalc();
+SeattlePublicLibrary.pndPerCup();
+SeattlePublicLibrary.netPndCalc();
+SeattlePublicLibrary.totalNetPndCalc();
+SeattlePublicLibrary.totalCustCalc();
+SeattlePublicLibrary.numEmpCalc();
+SeattlePublicLibrary.render();
+console.log('Number of employees needed at Seattle Public Library for each hour:',this.numEmp);
+
+// Adding the Seattle Public Library information to data.html
+// for (var i = 0; i < hours.length; i++) {
+//   var liEl = document.createElement('li');
+//   liEl.textContent = hours[i] + ': ' + SeattlePublicLibrary.netPnd[i] + ' lbs [' + SeattlePublicLibrary.customerHour[i] + ' customers, ' + SeattlePublicLibrary.cupsHr[i] + ' cups (' + SeattlePublicLibrary.pndPerCup[i] + ' lbs), ' + SeattlePublicLibrary.pndHR[i] + ' lbs to-go]';
+//   SeattlePublicLibrary.ulEl.appendChild(liEl);
+// }
+    // liEl[15] = document.createElement('li');
+    // liEl[15].textContent = 'Total customers at Capitol Hill: ' + this.totalCust;
+    // this.ulEl.appendChild(liEl[15]);
+    // liEl[16] = document.createElement('li');
+    // liEl[16].textContent = 'Total cups sold at Capitol Hill: ' + this.totalCups;
+    // this.ulEl.appendChild(liEl[16]);
+    // liEl[17] = document.createElement('li');
+    // liEl[17].textContent = 'Total to-go pound packages sold at Capitol Hill: ' + this.totalPnd;
+    // this.ulEl.appendChild(liEl[17]);
+    // liEl[18] = document.createElement('li');
+    // liEl[18].textContent = 'Total pounds of beans needed at Capitol Hill: ' + this.totalNetPnd;
+    // this.ulEl.appendChild(liEl[18]);
+
+SeattlePublicLibrary.render();
